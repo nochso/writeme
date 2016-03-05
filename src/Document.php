c85d86aee405154983804edc077361e51a05fe6d
Marcel Voigt <mv@noch.so>
2016-03-05 23:32:34 +0100

    Use Frontmatter class in Document

--- src/Document.php
+++ src/Document.php
@@ -1,15 +1,12 @@
 <?php
 namespace nochso\WriteMe;
 
-use nochso\Omni\EOL;
-use Symfony\Component\Yaml\Yaml;
-
 class Document
 {
     const FRONTMATTER_SEPARATOR = '---';
 
     /**
-     * @var array
+     * @var \nochso\WriteMe\Frontmatter
      */
     private $frontmatter;
     /**
@@ -22,11 +19,12 @@ class Document
      */
     public function __construct($content)
     {
-        $this->extract($content);
+        $this->frontmatter = new Frontmatter();
+        $this->content = $this->frontmatter->extract($content);
     }
 
     /**
-     * @return array
+     * @return \nochso\WriteMe\Frontmatter
      */
     public function getFrontmatter()
     {
@@ -47,32 +45,5 @@ class Document
     public function setContent($content)
     {
         $this->content = $content;
-    }
-
-    /**
-     * @param $content
-     */
-    private function extract($content)
-    {
-        $sections = explode(self::FRONTMATTER_SEPARATOR, $content);
-        $count = count($sections);
-        $frontmatter = [];
-        if ($count >= 2) {
-            $frontmatter = $sections[1];
-        }
-        if ($count >= 3) {
-            // Implode all remaining sections so `---` can be used in the content.
-            $this->content = implode(self::FRONTMATTER_SEPARATOR, array_slice($sections, 2));
-            // Trim only the first line ending
-            $eol = (string) EOL::detectDefault($this->content);
-            $len = strlen($eol);
-            if (substr($this->content, 0, $len) === $eol) {
-                $this->content = substr($this->content, $len);
-            }
-        }
-        if (is_string($frontmatter)) {
-            $frontmatter = Yaml::parse($frontmatter, true);
-        }
-        $this->frontmatter = $frontmatter;
     }
 }
