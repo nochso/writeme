4e77433164f8354e76dfdf3514ca62edc56ba68a
Marcel Voigt <mv@noch.so>
2016-03-11 21:44:59 +0100

    feat: Add class MarkdownTemplate
    Placeholders like the API placeholders need to create more complex
    Markdown. This class helps create custom Template classes that are used
    in pure PHP templates.
    
    This way common things like indenting are kept in one place and a
    unified style can be used.

---
+++ src/MarkdownTemplate.php
@@ -0,0 +1,82 @@
+<?php
+namespace nochso\WriteMe;
+
+use nochso\Omni\Path;
+
+/**
+ * MarkdownTemplate to help write Markdown templates in pure PHP.
+ */
+abstract class MarkdownTemplate
+{
+    const INDENT_TAB = "\t";
+    const INDENT_SPACE_TWO = ' ';
+    const INDENT_SPACE_FOUR = '    ';
+
+    protected $baseFolder = __DIR__ . '/../asset';
+    private $headerStartLevel = 0;
+    private $indentStartLevel = 0;
+    private $indentStyle = self::INDENT_SPACE_FOUR;
+
+    /**
+     * @param string $filepath
+     *
+     * @return string
+     */
+    public function render($filepath)
+    {
+        $path = Path::combine($this->baseFolder, $filepath);
+        ob_start();
+        include $path;
+        return ob_get_clean();
+    }
+
+    /**
+     * @param int    $level
+     * @param string $text  Optional.
+     *
+     * @return string
+     */
+    public function header($level, $text = '')
+    {
+        $header = str_repeat('#', $this->headerStartLevel + $level);
+        $headerText = sprintf('%s %s', $header, $text);
+        // Trim trailing space if $text is empty.
+        return rtrim($headerText, ' ');
+    }
+
+    /**
+     * @param int $indentStartLevel
+     */
+    public function setIndentStartLevel($indentStartLevel)
+    {
+        $this->indentStartLevel = $indentStartLevel;
+    }
+
+    /**
+     * @param int $level
+     */
+    public function setHeaderStartLevel($level)
+    {
+        $this->headerStartLevel = $level;
+    }
+
+    /**
+     * @param string $indentStyle
+     */
+    public function setIndentStyle($indentStyle)
+    {
+        $this->indentStyle = $indentStyle;
+    }
+
+    /**
+     * @param int    $level
+     * @param string $text
+     *
+     * @return string
+     */
+    public function indent($level, $text = '')
+    {
+        $indent = str_repeat($this->indentStyle, $this->indentStartLevel + $level);
+        return $indent . $text;
+    }
+}
