b09abdbae24649bfdb2e5cc0ba74de1eb5a54e27
Marcel Voigt <mv@noch.so>
2016-03-12 14:08:09 +0100

    feat: Add AbstractPlaceholder
    AbstractPlaceholder automatically prepares Placeholder specific options
    when its apply method is called.
    
    Inheriting classes MUST call `parent::apply($document)`

---
+++ src/Placeholder/AbstractPlaceholder.php
@@ -0,0 +1,44 @@
+<?php
+namespace nochso\WriteMe\Placeholder;
+
+use nochso\WriteMe\Document;
+use nochso\WriteMe\Interfaces\Placeholder;
+
+/**
+ * AbstractPlaceholder.
+ */
+abstract class AbstractPlaceholder implements Placeholder
+{
+    /**
+     * @var OptionList
+     */
+    protected $options;
+
+    /**
+     * @return string
+     */
+    abstract public function getIdentifier();
+
+    /**
+     * Apply must be called by inheriting classes, otherwise options won't be prepared.
+     *
+     * @param \nochso\WriteMe\Document $document
+     */
+    public function apply(Document $document)
+    {
+        $this->options = $this->getOptions();
+        $this->options->prepare($document->getFrontmatter());
+    }
+
+    /**
+     * getOptions returns a list of options that are used by this placeholder.
+     *
+     * You should not use this directly. Instead access the options property after calling `parent::apply`.
+     *
+     * @return \nochso\WriteMe\Placeholder\OptionList
+     */
+    public function getOptions()
+    {
+        return new OptionList([]);
+    }
+}
