9c9e1fcbd590817fb09b3ea12588d52263d8660a
Marcel Voigt <mv@noch.so>
2016-03-12 12:27:58 +0100

    feat: Add extractHeaderContents to Parser
    Returns a HeaderList containing HeaderContent objects.

--- src/Markdown/Parser.php
+++ src/Markdown/Parser.php
@@ -37,6 +37,45 @@ class Parser
     }
 
     /**
+     * extractHeaderContents returns a HeaderList containing HeaderContent objects.
+     *
+     * @param \nochso\WriteMe\Document $document
+     *
+     * @return \nochso\WriteMe\Markdown\HeaderList
+     *
+     * @todo Refactor this as it mostly duplicates extractHeaders
+     */
+    public function extractHeaderContents(Document $document)
+    {
+        $headerList = new HeaderList();
+        $lines = Multiline::create($document->getContent());
+        $prevLine = null;
+        $isFenced = false;
+        $currentHeaderContent = null;
+        $isNewHeader = false;
+        foreach ($lines as $line) {
+            if (preg_match('/^```(?!`)/', $line)) {
+                $isFenced = !$isFenced;
+            }
+            if (!$isFenced) {
+                $header = $this->extractHeader($line, $prevLine);
+                if ($header !== null) {
+                    $currentHeaderContent = HeaderContent::fromHeader($header);
+                    $headerList->add($currentHeaderContent);
+                    $isNewHeader = true;
+                }
+            }
+            // Add content only if it's *after* the header line
+            if ($currentHeaderContent !== null && !$isNewHeader) {
+                $currentHeaderContent->addContent($line);
+            }
+            $prevLine = $line;
+            $isNewHeader = false;
+        }
+        return $headerList;
+    }
+
+    /**
      * @param string      $line
      * @param string|null $prevLine
      *
