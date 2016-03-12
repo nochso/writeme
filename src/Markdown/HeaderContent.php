934acf32725cfba9246204a49c38729462c79c70
Marcel Voigt <mv@noch.so>
2016-03-12 12:26:17 +0100

    feat: Add HeaderContent extending from Header
    A HeaderContent combines a Header with the content following the header.

---
+++ src/Markdown/HeaderContent.php
@@ -0,0 +1,47 @@
+<?php
+namespace nochso\WriteMe\Markdown;
+
+/**
+ * HeaderContent represents a Markdown header and the content it contains.
+ */
+class HeaderContent extends Header
+{
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @param string $line
+     */
+    public function addContent($line)
+    {
+        $this->content .= $line . "\n";
+    }
+
+    /**
+     * @return string
+     */
+    public function toMarkdown()
+    {
+        return sprintf("%s\n%s", parent::toMarkdown(), $this->content);
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param \nochso\WriteMe\Markdown\Header $header
+     *
+     * @return \nochso\WriteMe\Markdown\HeaderContent
+     */
+    public static function fromHeader(Header $header)
+    {
+        return new self($header->getLevel(), $header->getText());
+    }
+}
