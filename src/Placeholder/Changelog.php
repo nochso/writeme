5ae2083f20010da91517f28c590c78db380b6d2d
Marcel Voigt <mv@noch.so>
2016-03-12 12:32:14 +0100

    feat: Extract latest changelog entries

--- src/Placeholder/Changelog.php
+++ src/Placeholder/Changelog.php
@@ -2,12 +2,17 @@
 namespace nochso\WriteMe\Placeholder;
 
 use Nette\Utils\Finder;
+use nochso\WriteMe\Converter;
 use nochso\WriteMe\Document;
 use nochso\WriteMe\Interfaces\Placeholder;
+use nochso\WriteMe\Markdown\HeaderContent;
+use nochso\WriteMe\Markdown\Parser;
 
 class Changelog implements Placeholder
 {
     const FILE_NAME_DEFAULT = 'CHANGELOG.md';
+    const MAX_CHANGES_DEFAULT = 2;
+    const RELEASE_LEVEL_DEFAULT = 2;
 
     /**
      * @return string
@@ -23,6 +28,30 @@ class Changelog implements Placeholder
     public function apply(Document $document)
     {
         $changelogPath = $this->findChangelog($document);
+        $changelog = Document::fromFile($changelogPath);
+        $parser = new Parser();
+        $headerContentList = $parser->extractHeaderContents($changelog);
+        $maxChanges = $document->getFrontmatter()->get('changelog.max-changes', self::MAX_CHANGES_DEFAULT);
+        $releaseLevel = $document->getFrontmatter()->get('changelog.release-level', self::RELEASE_LEVEL_DEFAULT);
+        $changes = 0;
+        $latestChanges = '';
+        /** @var HeaderContent $headerContent */
+        foreach ($headerContentList->getHeaders() as $headerContent) {
+            // This header marks a release
+            if ($headerContent->getLevel() === $releaseLevel) {
+                $changes++;
+                // Stop if we reached the max amount of changes.
+                if ($changes > $maxChanges) {
+                    break;
+                }
+                $latestChanges .= $headerContent->toMarkdown() . "\n";
+            }
+            // Keep adding sub-HeaderContent if we're within a release
+            if ($changes > 0 && $headerContent->getLevel() > $releaseLevel) {
+                $latestChanges .= $headerContent->toMarkdown() . "\n";
+            }
+        }
+        Converter::replace($this, $latestChanges, $document);
     }
 
     /**
