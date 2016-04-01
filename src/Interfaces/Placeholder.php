ee4e10d2b4f7900013914f7f56161ebfc7abe12d
Marcel Voigt <mv@noch.so>
2016-04-01 21:48:07 +0200

    refactor: Clean up the Placeholder interface

--- src/Interfaces/Placeholder.php
+++ src/Interfaces/Placeholder.php
@@ -1,7 +1,6 @@
 <?php
 namespace nochso\WriteMe\Interfaces;
 
-use nochso\WriteMe\Document;
 use nochso\WriteMe\Placeholder\Call;
 
 interface Placeholder
@@ -13,28 +12,18 @@ interface Placeholder
      * recognized as a placeholder. This **overrides** default behaviour of
      * freely defining placeholders in the frontmatter: `@motd@` will not be
      * replaced by its frontmatter content. Instead the placeholder needs to
-     * modify the document itself in method `apply`.
+     * modify the document itself when being `call`ed.
      *
      * @return string
      */
     public function getIdentifier();
 
     /**
-     * Apply changes to the content of a document.
-     *
-     * @param \nochso\WriteMe\Document $document
-     *
-     * @todo Remove this from the interface, instead use `call`
-     */
-    public function apply(Document $document);
-
-    /**
      * Call a method on the placeholder and expect it to modify the document.
      *
-     * @param \nochso\WriteMe\Placeholder\Call $call     Contains an optional method name and parameters
-     * @param \nochso\WriteMe\Document         $document The document to modify
+     * @param \nochso\WriteMe\Placeholder\Call $call Contains an optional method name and parameters
      */
-    public function call(Call $call, Document $document);
+    public function call(Call $call);
 
     /**
      * getDefaultOptionList returns the list of **default** options that are used by this placeholder.
