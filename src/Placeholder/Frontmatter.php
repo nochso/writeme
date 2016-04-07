f609a20363fde3dfbdaf752024bb6afd2244598d
Marcel Voigt <mv@noch.so>
2016-04-07 20:28:56 +0200

    docs: Document Frontmatter placeholder

--- src/Placeholder/Frontmatter.php
+++ src/Placeholder/Frontmatter.php
@@ -3,6 +3,32 @@ namespace nochso\WriteMe\Placeholder;
 
 use nochso\WriteMe\Interfaces\Placeholder;
 
+/**
+ * Frontmatter placeholders return values defined in the frontmatter.
+ *
+ * You can define any kind of structure as long as it doesn't collide with the name of any other available placeholder:
+ *
+ * ```yaml
+ * ---
+ * greet: Hello
+ * user:
+ *     name: [Annyong, Tobias]
+ * key.has.dots: yes
+ * ---
+ * > @greet@ @user.name.0@!
+ * key has dots: @key\.has\.dots@
+ * ```
+ *
+ * Frontmatter values are accessed using dot-notation, resulting in this output:
+ *
+ * ```markdown
+ * > Hello Annyong!
+ * key has dots: yes
+ * ```
+ *
+ * Using dots in the keys themselves is possible by escaping them with backslashes. See the `Dot` class provided by
+ * [nochso/omni](https://github.com/nochso/omni).
+ */
 class Frontmatter extends AbstractPlaceholder
 {
     /**
