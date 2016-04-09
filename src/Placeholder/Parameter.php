6749a8571660e7427c9870a17852463072bf9dd8
Marcel Voigt <mv@noch.so>
2016-04-09 18:06:59 +0200

    feat: Add Parameter class for easier reflection

---
+++ src/Placeholder/Parameter.php
@@ -0,0 +1,61 @@
+<?php
+namespace nochso\WriteMe\Placeholder;
+
+use BetterReflection\Reflection\ReflectionParameter;
+use nochso\WriteMe\Markdown\DocBlock;
+
+class Parameter
+{
+    /**
+     * @var \BetterReflection\Reflection\ReflectionParameter
+     */
+    private $reflectionParameter;
+
+    public function __construct(ReflectionParameter $reflectionParameter)
+    {
+        $this->reflectionParameter = $reflectionParameter;
+    }
+
+    /**
+     * @return \BetterReflection\Reflection\ReflectionParameter
+     */
+    public function getReflectionParameter()
+    {
+        return $this->reflectionParameter;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->reflectionParameter->getName();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHints()
+    {
+        $hints = $this->reflectionParameter->getDocBlockTypeStrings();
+        $hints[] = $this->reflectionParameter->getTypeHint();
+        $hints = array_filter($hints, 'strlen');
+        return implode('|', $hints);
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription()
+    {
+        $docBlock = new DocBlock($this->reflectionParameter->getDeclaringFunction()->getDocComment());
+        /** @var \phpDocumentor\Reflection\DocBlock\Tag\ParamTag[] $paramTags */
+        $paramTags = $docBlock->getTagsByName('param');
+        foreach ($paramTags as $paramTag) {
+            if ($paramTag->getVariableName() === '$' . $this->reflectionParameter->getName()) {
+                return $paramTag->getDescription();
+            }
+        }
+        return '';
+    }
+}
